01c77a1900a93dd9c9e9a2408c19399d9634f523
Flegel <austin.flegel.760@k12.friscoisd.org>
2022-12-19 13:47:25 -0600

    Update WalkingEnemy.cs

--- KungFuMaster/KungFuMaster/WalkingEnemy.cs
+++ KungFuMaster/KungFuMaster/WalkingEnemy.cs
@@ -9,9 +9,13 @@ namespace KungFuMaster
 {
 	class WalkingEnemy : Entity
 	{
+		private Rectangle rect;
+		private Texture2D texture;
+
 		public WalkingEnemy(Rectangle rect, Texture2D texture) : base(rect, texture)
 		{
-
+			this.rect = rect;
+			this.texture = texture;
 		}
 
 		public override void Update()
